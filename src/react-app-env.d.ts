4c186c28ce0215abf9c452c5a6b551aa8dbd6f46
Денис Мериоара <merioara@megapolis-it.ru>
2022-02-22 10:47:05 +0300

    feat: init

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="react-scripts" />
+
+declare type AppEnv = {
+    NODE_ENV: string;
+
+    /** Ключ от карты */
+    REACT_APP_MAP_API_KEY: string;
+    
+    /** Координаты центра по умолчанию */
+    REACT_APP_MAP_CENTER: [number, number];
+
+    /** Ссылка на таил */
+    REACT_APP_MAP_CADASTRAL_DIVISION_TILE: string;
+  };
